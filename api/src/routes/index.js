edc2f31eda47a2fcd8871fb9fb80412006b07c07
Tomas <tomasbarale1@gmail.com>
2022-10-12 17:28:25 -0300

    tomas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,10 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
 
 module.exports = router;
