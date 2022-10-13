672c263293c38844c5a481e3c45f037f2a4f365a
Tomas <tomasbarale1@gmail.com>
2022-10-13 12:48:52 -0300

    relaciones

--- api/src/models/payment_detail.js
+++ api/src/models/payment_detail.js
@@ -1,23 +1,26 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('paymentDetail', {
-      amount:{
-        type:DataTypes.INTEGER,
-        allownull:false,
+  sequelize.define(
+    "paymentDetail",
+    {
+      amount: {
+        type: DataTypes.INTEGER,
+        allownull: false,
       },
-      provider:{
-          type:DataTypes.STRING,
-          allownull:false,
+      provider: {
+        type: DataTypes.STRING,
+        allownull: false,
       },
-      state:{
-          type:DataTypes.BOOLEAN,
-          allownull:false,
+      state: {
+        type: DataTypes.BOOLEAN,
+        allownull: false,
       },
     },
     {
-      timestamps: false
+      timestamps: false,
     }
-  )}
+  );
+};
