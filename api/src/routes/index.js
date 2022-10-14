d2f2b50fc706de2e49066dfbb881508f03797d2d
Mateo <mateoreynoso2009@gmail.com>
2022-10-14 19:41:53 -0300

    get brand y get cpu brand

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,6 +5,8 @@ const detail = require("../Controller/get_detail");
 const post = require("../Controller/post_products");
 const type = require("../Controller/get_type");
 const price = require("../Controller/get_price");
+const brand = require("../Controller/get_brand");
+const cpuBrand = require("../Controller/get_cpuBrand")
 const router = Router();
 
 // Configurar los routers
@@ -14,5 +16,7 @@ router.use("/productDetail/", detail);
 router.use("/postProduct", post);
 router.use("/productType", type);
 router.use("/productPrice", price);
+router.use("/brand", brand);
+router.use("/cpuBrand", cpuBrand);
 
 module.exports = router;
