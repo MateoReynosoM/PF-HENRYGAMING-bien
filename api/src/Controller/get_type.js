ffba629c725a7cd1f643cb5d149e58d3fcbb2db6
Mateo <mateoreynoso2009@gmail.com>
2022-10-14 18:14:04 -0300

    cambios get_type y cambios modelo product

--- api/src/Controller/get_type.js
+++ api/src/Controller/get_type.js
@@ -4,20 +4,22 @@ const { Product } = require("../db");
 const getType = Router();
 
 getType.get("/", async (req, res, next) => {
-  const { id } = req.query;
+  const { type } = req.query;
 
-  if (!id)
+  if (!type)
     return res.status(404).json({ message: "No se econtro ese Type." });
 
   try {
+    /* let productsType = products.filter((el) => el.categoryId==id ); */
     let products = await Product.findAll({
       attributes: ["id", "img", "type", "model", "price", "detail", "brandId", "categoryId"]
     });
-    let productsType = products.filter((el) => el.categoryId==id );
+    let productsType = products.filter((el) =>
+      el.type.toLowerCase().includes(type.toLowerCase()))
 
     return productsType.length
       ? res.status(200).send(productsType)
-      : res.status(404).send("No se recibio un id correcto");
+      : res.status(404).send("No se recibio un type correcto");
   } catch (error) {
     console.error(error);
     next();
