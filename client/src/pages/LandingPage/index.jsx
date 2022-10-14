942fede1e6c60dfb480a8053003f0c7c79c2d489
Benjamin Belpoliti <belpolitibenja@gmail.com>
2022-10-14 13:22:17 -0300

    Landing page alpha

--- client/src/pages/LandingPage/index.jsx
+++ client/src/pages/LandingPage/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useGetPartsQuery } from '../../redux/rtk-api'
+import styles from "./styles/LandingPage.css"
 
 function LandingPage() {
+  const {data, error, isLoading} = useGetPartsQuery()
   return (
-    <div>LandingPage</div>
+    <div id='landing'>
+      <Link to="/home"><img id='logo' src="logo.png" alt="Enter"></img></Link>
+    </div>
   )
 }
 
