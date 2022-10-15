3bb28b2aa6f09ea015998de769e54271d4fba4ea
Tomas <tomasbarale1@gmail.com>
2022-10-15 14:30:06 -0300

    sort a ruta allProducts

--- api/src/Controller/get_products.js
+++ api/src/Controller/get_products.js
@@ -4,13 +4,22 @@ const { Product } = require("../db");
 const getAllProducts = Router();
 
 getAllProducts.get("/", async (req, res, next) => {
-    try {
-        let allProducts = await Product.findAll();
-        return res.json(allProducts);
-    } catch (error) {
-        console.error(error);
-        next();
-    }
+  try {
+    let allProducts = await Product.findAll();
+    allProducts.sort(function (a, b) {
+      if (a.id > b.id) {
+        return 1;
+      }
+      if (b.id > a.id) {
+        return -1;
+      }
+      return 0;
+    });
+    return res.json(allProducts);
+  } catch (error) {
+    console.error(error);
+    next();
+  }
 });
 
 module.exports = getAllProducts;
