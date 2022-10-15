a833ff158127a88452b0bf8862219be8b0783b9b
EmanFun <emanuelfun.95@gmail.com>
2022-10-14 23:47:21 -0300

    git pull origin back 23:47

--- api/src/models/Category.js
+++ api/src/models/Category.js
@@ -3,11 +3,16 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("category", {
-    name: {
-      type: DataTypes.STRING,
-      allownull: false,
-
+  sequelize.define(
+    "category",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allownull: false,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
