a63dbcbde066af9a71485c925ff9b2446c6e8ba8
Benjamin Belpoliti <belpolitibenja@gmail.com>
2022-10-19 14:30:33 -0300

    home added again

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Carousel, Container } from 'react-bootstrap'
+import { useGetFeaturedProductsQuery } from '../../redux/rtk-api'
+import styles from "./styles/Home.css"
+
+// Banner
 
 
 function Home() {
+  const {data, error, isLoading} = useGetFeaturedProductsQuery()
   return (
-    <div>Home</div>
+        <Carousel>
+            {data?.map(p => (
+                <Carousel.Item className='mh-50 bg-dark'>
+                    <img
+                      className="w-25 d-block mx-auto carousel-image"
+                      src={p.img}
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                          <h3>{p.model}</h3>
+                          <p>USD {p.price}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            ))}
+        </Carousel>
   )
 }
 
