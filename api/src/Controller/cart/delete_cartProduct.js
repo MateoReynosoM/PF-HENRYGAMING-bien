30974028b801ef17c750250860bfae07c9a11823
EmanFun <emanuelfun.95@gmail.com>
2022-10-23 01:31:17 -0300

    apply discount when delete cart Product

--- api/src/Controller/cart/delete_cartProduct.js
+++ api/src/Controller/cart/delete_cartProduct.js
@@ -1,6 +1,6 @@
 const Router = require("express");
 const { where } = require("sequelize");
-const { CartProduct } = require("../../db");
+const { CartProduct, Product, Cart } = require("../../db");
 const { verifyToken } = require("../Utils/jwt_middlewares");
 
 //ejemplo de ruta http://localhost:3001/deleteCartProduct?id=1
@@ -11,11 +11,14 @@ deleteCartProduct.delete("/", verifyToken, async (req, res, next) => {
   const { id } = req.query;
 
   try {
-    let productDestroy = await CartProduct.destroy({
-      where: {
-        id: id,
-      },
-    });
+    let productDestroy = await CartProduct.findByPk(id);
+    let product = await Product.findByPk(productDestroy.productId)
+    let cart = await Cart.findByPk(productDestroy.cartId)
+    let discount = (product.price * productDestroy.amount)
+    cart.update({
+      total: (cart.total - discount)
+    })
+    
 
     if (productDestroy) return res.send("Product deleted successfully");
     return res.status(404).send("Product not found");
