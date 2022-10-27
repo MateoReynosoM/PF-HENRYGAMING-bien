dd7baae067730da5841fa94e585ca707034e9cf4
Benjamin Belpoliti <belpolitibenja@gmail.com>
2022-10-27 03:11:27 -0300

    urls success/fail/pending mercado pago

--- api/src/Mercadopago/Services/PaymentService.js
+++ api/src/Mercadopago/Services/PaymentService.js
@@ -1,38 +1,35 @@
 const axios = require("axios");
 
 class PaymentService {
-  async createPayment( productList) {
-    const url = "https://api.mercadopago.com/checkout/preferences";
-    
-    
-    const body = {
-      items: productList[0].cartProducts.map((e) =>
-        ({
-          title: `${e.product.brand.name} ${e.product.model}`,
-          unit_price: e.product.price,
-          quantity: e.amount
-        })
-        )
-      ,
-      back_urls: {
-        success: "/success",
-        failure: "/failure",
-        pending: "/pending",
-      },
-      auto_return: "approved",
-    };
+    async createPayment(productList) {
+        const url = "https://api.mercadopago.com/checkout/preferences";
 
-    const payment = await axios.post(url, body, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
-      }
-    });
+        // Hay que actualizar back_urls para que pueda tomar localhost o la url de vercel antes de mergear a development
 
-    return payment.data;
-  }
+        const body = {
+            items: productList[0].cartProducts.map((e) => ({
+                id: e.product.id,
+                title: `${e.product.brand.name} ${e.product.model}`,
+                unit_price: e.product.price,
+                quantity: e.amount,
+            })),
+            back_urls: {
+                success: "http://localhost:3000/purchaseSuccess",
+                failure: "http://localhost:3000/purchaseFailiure",
+                pending: "http://localhost:3000/purchasePending",
+            },
+            auto_return: "approved",
+        };
 
-  
+        const payment = await axios.post(url, body, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+            },
+        });
+
+        return payment.data;
+    }
 }
 
 module.exports = PaymentService;
