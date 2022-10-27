da7fd7a6b9883cdebb6bccc6938f621ed3a33c86
Mateo <mateoreynoso2009@gmail.com>
2022-10-27 19:45:19 -0300

    payment detail terminado

--- api/src/Controller/Payment/post_paymentDetail.js
+++ api/src/Controller/Payment/post_paymentDetail.js
@@ -4,7 +4,7 @@ const { SECRET } = process.env;
 const { CartProduct, Cart, Product, PaymentDetail,PurchaseDetail ,PurchasedProduct} = require("../../db");
 const { verifyToken } = require("../Utils/jwt_middlewares");
 
-//ejemplo de ruta http://localhost:3001/
+//ejemplo de ruta http://localhost:3001/paymentDetail
 
 const postPaymentDetail = Router();
 
@@ -15,6 +15,7 @@ postPaymentDetail.post("/", verifyToken, async (req, res, next) => {
     var userId = req.userId;
 
         const {amount, provider, state} = req.body
+        if(state==="failure" || state==="pending")res.status(404).send({message:"Payment status is pending or failed, please try again later"})
         if(!amount&&!provider&&!state)res.status(404).send({message:"Some information was not provided"})
         const paymentData={
             amount,
@@ -32,23 +33,38 @@ postPaymentDetail.post("/", verifyToken, async (req, res, next) => {
         }
         )
         console.log(productosCarrito)
+        
+        let cart = await Cart.findOne({
+          where:{
+            userId:userId
+          }
+        })
+        console.log(cart);
+        const paymentDetail = await PaymentDetail.create(paymentData);
+
+        const purchaseDetail = await PurchaseDetail.create({total:cart.total,userId:userId,paymentDetailId:paymentDetail.id })
+
         var products = productosCarrito.map((m)=> 
-        {return{amount:m.amount, productId:m.productId}}
+        {return{amount:m.amount, productId:m.productId, purchaseDetailId:purchaseDetail.id }}
         )
 
-        
         const purchasedProducts = await PurchasedProduct.bulkCreate(products)
-        const paymentDetail = await PaymentDetail.create(paymentData);
-    
+
 
     try {
-        
-        
-
-
-
-    
-    return res.status(200).json([paymentDetail,purchasedProducts]);
+        let answer= await PaymentDetail.findAll({
+          where:{
+            userId:userId
+          },
+          include:{
+            model:PurchaseDetail,
+            include:{
+              model:PurchasedProduct
+            }
+          }
+        })
+  
+    return res.status(200).json(answer);
     
   } catch (error) {
     next(error);
