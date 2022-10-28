354f63967b7adce2d8d5e402222e036bb12326d2
Benjamin Belpoliti <belpolitibenja@gmail.com>
2022-10-28 01:16:44 -0300

    ahora anda con el back y cropea imagenes

--- client/src/pages/UserProfile/index.jsx
+++ client/src/pages/UserProfile/index.jsx
@@ -77,7 +77,7 @@ function User() {
         <Tabs defaultActiveKey="profile"
                 id="uncontrolled-tab-example"
                 className="mb-3">
-            <Tab eventKey="profile" title="Profile"><ProfileTab userName={user.userName} firstName={user.firstName} lastName={user.lastName} email={user.email}/></Tab>
+            <Tab eventKey="profile" title="Profile"><ProfileTab img={user.img} userName={user.userName} firstName={user.firstName} lastName={user.lastName} email={user.email}/></Tab>
             <Tab eventKey="adresses" title="Adresses"><UserAddresses/></Tab>
             <Tab eventKey="payment" title="Payment"></Tab>
             <Tab eventKey="history" title="Purchase History"><PurchaseHistory/></Tab>
