15964ab3e48588920b6565f27eb9fa8291865d1c
pablocana <pablocanavesi@hotmail.com>
2022-10-29 20:01:56 -0300

    prueba about us

--- client/src/pages/About Us/AboutUs.jsx
+++ client/src/pages/About Us/AboutUs.jsx
@@ -3,19 +3,32 @@ function AboutUs(){
     return(
         <>
             <div>
-                <h1>About Us</h1>
+                <h2>About Us</h2>
                 <p>
                     HenryGaming is an e-commerce website created in 2022 by HenryGaming Argentina SRL, where you can find the best market products 
                     and every hardware component you need to be at the forefront of computer technology.
                     You will be able to consult for personalized advice by sending a simple message as we require in the FAQs and coordinate with our team on the delivery system, changes and modifications to your orders.
                     Thanks for meeting us!
                 </p>
-                <h1>The Team</h1>
+                <h2>The Team</h2>
                 <p>
                     This website was made as a final project of the Full Stack Developer course taught by www.soyhenry.com. Made by the team shown below.
                     We give a special thanks to those who accompanied us in this project, among them, our assigned mentor Nicolas Burgueño and and our initial correction teacher Daiana Guillia.
                 </p>
             </div>
+            <div>	container integrantes	
+				<div>	cards group a
+					<div> <img href="..." alt="asd"> <div> <h5>name</h5> <p>FULL STACK WEB DEVELOPER</p> <p><img href="..." alt="asd"></p> </div> <div/> 1
+					<div> <img> <div> <h5>name</h5> <p>FULL STACK WEB DEVELOPER</p> <p><img></p> </div> <div/> 
+					<div> <img> <div> <h5>name</h5> <p>FULL STACK WEB DEVELOPER</p> <p><img></p> </div> <div/> 
+					<div> <img> <div> <h5>name</h5> <p>FULL STACK WEB DEVELOPER</p> <p><img></p> </div> <div/> 	
+				<div/>
+				<div>	cards group b
+					<div> <img> <div> <h5>name</h5> <p>FULL STACK WEB DEVELOPER</p> <p><img></p> </div> <div/> 	
+					<div> <img> <div> <h5>name</h5> <p>FULL STACK WEB DEVELOPER</p> <p><img></p> </div> <div/> 
+					<div> <img> <div> <h5>name</h5> <p>FULL STACK WEB DEVELOPER</p> <p><img></p> </div> <div/>
+				<div/>	
+			</div>
         </>
     )
 }
