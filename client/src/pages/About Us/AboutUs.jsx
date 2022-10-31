6c009742a8155f72f5f5d25e59409ec8f9ecd8d3
pablocana <pablocanavesi@hotmail.com>
2022-10-30 22:28:01 -0300

    cambio about

--- client/src/pages/About Us/AboutUs.jsx
+++ client/src/pages/About Us/AboutUs.jsx
@@ -1,3 +1,6 @@
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import "./about.css";
 
 function AboutUs(){
     return(
@@ -10,13 +13,111 @@ function AboutUs(){
                     You will be able to consult for personalized advice by sending a simple message as we require in the FAQs and coordinate with our team on the delivery system, changes and modifications to your orders.
                     Thanks for meeting us!
                 </p>
-                <h2>The Team</h2>
+                <h2>The Project</h2>
                 <p>
                     This website was made as a final project of the Full Stack Developer course taught by www.soyhenry.com. Made by the team shown below.
                     We give a special thanks to those who accompanied us in this project, among them, our assigned mentor Nicolas Burgueño and and our initial correction teacher Daiana Guillia.
                 </p>
             </div>
-            <div>	container integrantes	
+            <div>
+            <h2>The Team</h2>
+            <div className="pruebacontainer">
+                <div>
+                    <h4>Front Team</h4>
+                        <CardGroup>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="benja.png" style={{padding:'1rem', height:'100%', maxHeight: '10rem', objectFit: 'contain'}}/>
+                                <Card.Body>
+                                    <Card.Title>Benja</Card.Title>
+                                    <Card.Text>
+                                        FULL STACK WEB DEVELOPER
+                                    </Card.Text>
+                                    <Card.Link href="https://www.linkedin.com/signup/cold-join?trk=guest_homepage-basic_nav-header-join">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="benja.png" />
+                                <Card.Body>
+                                    <Card.Title>Benja2</Card.Title>
+                                    <Card.Text>
+                                        FULL STACK WEB DEVELOPER
+                                    </Card.Text>
+                                    <Card.Link href="#">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="benja.png" />
+                                <Card.Body>
+                                    <Card.Title>Benja</Card.Title>
+                                    <Card.Text>
+                                        FULL STACK WEB DEVELOPER
+                                    </Card.Text>
+                                    <Card.Link href="#">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="benja.png" />
+                                <Card.Body>
+                                    <Card.Title>Benja</Card.Title>
+                                    <Card.Text>
+                                        FULL STACK WEB DEVELOPER
+                                    </Card.Text>
+                                    <Card.Link href="#">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </CardGroup>
+                    </div>
+                <div>
+                    <h4>Back Team</h4>
+                        <CardGroup>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="benja.png" />
+                                <Card.Body>
+                                    <Card.Title>Benja</Card.Title>
+                                    <Card.Text>
+                                        FULL STACK WEB DEVELOPER
+                                    </Card.Text>
+                                    <Card.Link href="#">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="benja.png" />
+                                <Card.Body>
+                                    <Card.Title>Benja2</Card.Title>
+                                    <Card.Text>
+                                        FULL STACK WEB DEVELOPER
+                                    </Card.Text>
+                                    <Card.Link href="#">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="benja.png" />
+                                <Card.Body>
+                                    <Card.Title>Benja</Card.Title>
+                                    <Card.Text>
+                                        FULL STACK WEB DEVELOPER
+                                    </Card.Text>
+                                    <Card.Link href="#">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="benja.png" />
+                                <Card.Body>
+                                    <Card.Title>Benja</Card.Title>
+                                    <Card.Text>
+                                        FULL STACK WEB DEVELOPER
+                                    </Card.Text>
+                                    <Card.Link href="#">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </CardGroup>
+                    </div>
+                </div>
+            </div>    
+
+
+
+            {/* <div>	container integrantes	
 				<div>	cards group a
 					<div> <img href="..." alt="asd"> <div> <h5>name</h5> <p>FULL STACK WEB DEVELOPER</p> <p><img href="..." alt="asd"></p> </div> <div/> 1
 					<div> <img> <div> <h5>name</h5> <p>FULL STACK WEB DEVELOPER</p> <p><img></p> </div> <div/> 
@@ -28,7 +129,7 @@ function AboutUs(){
 					<div> <img> <div> <h5>name</h5> <p>FULL STACK WEB DEVELOPER</p> <p><img></p> </div> <div/> 
 					<div> <img> <div> <h5>name</h5> <p>FULL STACK WEB DEVELOPER</p> <p><img></p> </div> <div/>
 				<div/>	
-			</div>
+			</div> */}
         </>
     )
 }
