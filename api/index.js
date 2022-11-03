25534c0187e28d9ce63e3f2ea967532c428db482
Mateo Reynoso Marin <105386264+MateoReynosoM@users.noreply.github.com>
2022-11-03 16:11:05 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ const loadData = require("./src/Controller/Utils/load_data");
 const getFavorites = require("./src/Controller/Favorites/get_favorites.js");
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
     server.listen(process.env.PORT, async () => {
         loadData();
         //------testCart--------
