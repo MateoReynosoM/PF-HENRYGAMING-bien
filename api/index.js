1f874a8da2fc2247ed5dc0d8238aca9301fa983d
Benjamin Belpoliti <belpolitibenja@gmail.com>
2022-11-03 00:27:01 -0300

    asd

--- api/index.js
+++ api/index.js
@@ -19,21 +19,21 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const { User, CartProduct,Favorites,Cart } = require("./src/db");
+const { User, CartProduct, Favorites, Cart } = require("./src/db");
 
 const loadData = require("./src/Controller/Utils/load_data");
 const getFavorites = require("./src/Controller/Favorites/get_favorites.js");
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, async () => {
-    loadData();
-    //------testCart--------   
-    
-    await User.create(JSON.parse(process.env.ADMIN_USER));
-    await Favorites.create({amount:0,userId: 1})
-    await Cart.create({total: 0, userId: 1})
+    server.listen(process.env.PORT, async () => {
+        loadData();
+        //------testCart--------
 
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
-  });
+        /* await User.create(JSON.parse(process.env.ADMIN_USER));
+    await Favorites.create({amount:0,userId: 1})
+    await Cart.create({total: 0, userId: 1}) */
+
+        console.log("%s listening at 3001"); // eslint-disable-line no-console
+    });
 });
