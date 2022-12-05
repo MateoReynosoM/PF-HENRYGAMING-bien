1c1bd45cc24989fbf40327a72cddb68367ad9fcb
Mateo Reynoso Marin <105386264+MateoReynosoM@users.noreply.github.com>
2022-12-05 20:36:46 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, PGPORT } = process.env;
 
 let sequelize =
   process.env.NODE_ENV === "production"
@@ -10,7 +10,7 @@ let sequelize =
         database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
-        port: 5432,
+        port: PGPORT,
         username: DB_USER,
         password: DB_PASSWORD,
         pool: {
